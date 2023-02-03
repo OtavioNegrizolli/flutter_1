2f9b3baa5dabfd8eddf4751cd83e8099c975f143
Otávio Negrizolli <otavionegrizolli@hotmail.com>
2023-02-03 11:57:17 -0300

    add ios splash screen

--- lib/ios/ios.app.dart
+++ lib/ios/ios.app.dart
@@ -1,17 +1,19 @@
+import 'package:contacts/ios/style.dart';
+import 'package:contacts/ios/views/splash.view.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class IOSApp extends StatelessWidget {
   const IOSApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      title: 'Flutter Demo',
-      theme: CupertinoThemeData(
-        primaryColor: Colors.blue,
+    return CupertinoApp(
+      title: 'Contacts',
+      debugShowCheckedModeBanner: false,
+      theme: iosTheme(),
+      home: const CupertinoPageScaffold(
+        child: SplashView(),
       ),
-      home: Text('Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/ios/views/splash.view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:contacts/android/styles.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: primaryColor,
+      // ignore: avoid_unnecessary_containers
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            SizedBox(width: double.infinity),
+            Icon(
+              CupertinoIcons.padlock,
+              size: 72,
+              color: accentColor,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Contatos",
+              style: TextStyle(color: accentColor),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
